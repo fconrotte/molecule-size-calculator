735571ae84563a56321571f9a2d98ba5a11e1f45
destris01 <46545841+destris01@users.noreply.github.com>
2019-11-26 17:26:49 +0100

    added js

---
+++ script.js
@@ -0,0 +1,49 @@
+const weightInput = document.querySelector('.weight');
+const heightInput = document.querySelector('.height');
+const result = document.querySelector('.result');
+
+
+
+
+
+   
+    
+    heightInput.addEventListener('input', function() {
+        const heightValue = heightInput.value;
+        const weightValue = weightInput.value;
+       
+        const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
+
+        if (heightValue.toString().length === 3) {
+            result.innerHTML = (bmi.toFixed(1));
+        }
+        
+        
+        
+        
+        
+        console.log(bmi);
+    });
+
+    
+
+    
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
