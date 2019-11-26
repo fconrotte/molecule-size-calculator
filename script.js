36e30ba261ee2fa5c23ed0b79026e396f21f340c
destris01 <46545841+destris01@users.noreply.github.com>
2019-11-26 19:54:49 +0100

    function for cleaning result

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const weightInput = document.querySelector('.weight');
 const heightInput = document.querySelector('.height');
 const result = document.querySelector('.result');
+const clrBtn = document.querySelector('.reset');
 
 
-        heightInput.addEventListener('input', function() {
-        const heightValue = heightInput.value;
-        const weightValue = weightInput.value;
-       
-        const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
 
-        if (heightValue.toString().length === 3) {
+
+
+heightInput.addEventListener('input', function() {
+    const heightValue = heightInput.value;
+    const weightValue = weightInput.value;
+    const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
+
+    if (heightValue.toString().length === 3) {
             result.innerHTML = (bmi.toFixed(1));
         }
-        
-        
-        
-        
-        
-        console.log(bmi);
-    });
+            
+            
+            
+            
+            
+    console.log(bmi);
+});
+
+clrBtn.addEventListener('click', function() {
+    result.innerHTML = "";
+});
+
+
+
+
 
     
 
