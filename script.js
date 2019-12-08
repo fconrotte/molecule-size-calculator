c545f97f943ce4e1c8e85d1f13603b251acf79a3
destris01 <46545841+destris01@users.noreply.github.com>
2019-12-08 18:33:20 +0100

    fixed bugs

--- script.js
+++ script.js
@@ -6,34 +6,57 @@ const clrBtn = document.querySelector('#reset');
 
 
 
+weightInput.addEventListener('input', function() {
+    const heightValue = heightInput.value;
+    const weightValue = weightInput.value;
+    const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
+
+    if (weightValue >= 30  &&  weightValue <=200) {
+        
+        result.innerHTML = (bmi.toFixed(1));
+
+        if (result.innerHTML = Infinity) {
+            result.innerHTML = "Enter your height";
+        }
+                
+    }
+
+    
+    else {
+        result.innerHTML = "Uncorrect weight (min 30, max 200)"
+    }
+
+    if (weightValue.toString().length === 0) {
+        result.innerHTML = "Enter your weight";
+    }
+});
+
 
 heightInput.addEventListener('input', function() {
     const heightValue = heightInput.value;
     const weightValue = weightInput.value;
     const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
 
-    if (heightValue.toString().length === 3) {
+    if (heightValue >=120 && heightValue <=230) {
         result.innerHTML = (bmi.toFixed(1));
     }
+
+    else {
+        result.innerHTML = "Uncorrect height (min 120, max 230)"
+    }
+
+    if (heightValue.toString().length === 0) {
+        result.innerHTML = "Enter your height";
+
+        
+    }
     
-            
-            
-            
-            
-            
-    console.log(bmi);
+                   
+    
 });
 
 
-weightInput.addEventListener('input', function() {
-    const heightValue = heightInput.value;
-    const weightValue = weightInput.value;
-    const bmi = weightValue / Math.pow(heightValue / 10, 2) * 100;
 
-    if (weightValue.toString().length === 2 && heightValue.toString().length === 3) {
-        result.innerHTML = (bmi.toFixed(1));
-    } 
-});
 
 
 clrBtn.addEventListener('click', function() {
@@ -41,27 +64,6 @@ clrBtn.addEventListener('click', function() {
 });
 
 
-
-
-
-    
-
-    
-
-    
-
-    
-    
-
-
-
-
-
-
-
-
-
-    
 
 
 
