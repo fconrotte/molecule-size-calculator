638d503c3635636491347f4d0651cb395b67a39f
fconrotte <fconrotte@users.noreply.github.com>
2020-04-30 12:30:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const result = document.querySelector('#result');
 const clrBtn = document.querySelector('#reset');
 
 oildropdiameterInput.addEventListener('input', function() {
-    const oilpuddlediameterValue = oilpuddlediameterInput.value;
+	const oilpuddlediameterValue = document.getElementById("oilpuddlediameter").value;
     const oildropdiameterValue = oildropdiameterInput.value;
+
+    if (oilpuddlediameterValue.toString().length === 0 || oilpuddlediameterValue.value == 0) {
+        result.innerHTML = "Enter the oil puddle diameter";
+		return;
+    }
     
     // h = 4/3 (drop diameter/2)³ / (puddle diameter/2)²
     
-    const moleculesize = (4/3 * Math.pow(oildropdiameterValue/2,3)) / Math.pow(oilpuddlediameterValue*10/2,2);  
+    const moleculesize = (4/3 * Math.pow((oildropdiameterValue/2),3)) / Math.pow(((oilpuddlediameterValue*10)/2),2);  
 
-    if (oildropdiameterValue >= 1 && oildropdiameterValue <=10) {
-        result.innerHTML = (moleculesize.toFixed(10));
-
-        if (result.innerHTML = Infinity) {
-            result.innerHTML = "Enter the oil puddle diameter";
-        }
+    if (oildropdiameterValue >= 0.1 && oildropdiameterValue <=20) {
+        result.innerHTML = (moleculesize.toFixed(10)) + " millimeter<br/><br/>" + (moleculesize.toFixed(10))*1000 + " micrometer<br/><br/>" + (moleculesize.toFixed(10))*1000000 + " nanometer<br/><br/>";
     }
     else
     {
@@ -31,12 +32,17 @@ oildropdiameterInput.addEventListener('input', function() {
 
 oilpuddlediameterInput.addEventListener('input', function() {
     const oilpuddlediameterValue = oilpuddlediameterInput.value;
-    const oildropdiameterValue = oildropdiameterInput.value;
+	const oildropdiameterValue = document.getElementById("oildropdiameter").value;
+
+    if (oildropdiameterValue.toString().length === 0 || oildropdiameterValue.value == 0) {
+        result.innerHTML = "Enter the oil drop diameter";
+		return;
+    }
     
-    const moleculesize = (4/3 * Math.pow(oildropdiameterValue/2,3)) / Math.pow(oilpuddlediameterValue*10/2,2);  
+    const moleculesize = (4/3 * Math.pow((oildropdiameterValue/2),3)) / Math.pow(((oilpuddlediameterValue*10)/2),2);  
 
     if (oilpuddlediameterValue >=1 && oilpuddlediameterValue <= 30) {
-        result.innerHTML = (moleculesize.toFixed(10));
+        result.innerHTML = (moleculesize.toFixed(10)) + " millimeter<br/><br/>" + (moleculesize.toFixed(10))*1000 + " micrometer<br/><br/>" + (moleculesize.toFixed(10))*1000000 + " nanometer<br/><br/>";
     }
     else
     {
